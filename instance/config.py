3036395d9fa27eb02a9731d46ab600694ae76959
Innocent Asiimwe <asiimwe@outlook.com>
2017-08-30 23:22:03 +0300

    Add classes to handle app configurations at the different stages

---
+++ instance/config.py
@@ -0,0 +1,35 @@
+"""Module contains configurations for the app"""
+import os
+
+class Config:
+    """The Parent configurations for the app"""
+    DEBUG = False
+    CSRF_ENABLED = True
+    SECRET = os.urandom(24)
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:admin@localhost/bucketlist_api'
+
+class DevelopmentConfig(Config):
+    """Class for development configurations"""
+    DEBUG = True
+
+class TestingConfig(Config):
+    """Class for the testing configurations"""
+    DEBUG = True
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:admin@localhost/test_db'
+
+class StagingConfig(Config):
+    """Class for the staging configurations"""
+    DEBUG = True
+
+class ProductionConfig(Config):
+    """Class for the Production configurations"""
+    DEBUG = False
+    TESTING = False
+
+app_config = {
+    'development':DevelopmentConfig,
+    'testing':TestingConfig,
+    'staging':StagingConfig,
+    'production':ProductionConfig
+}

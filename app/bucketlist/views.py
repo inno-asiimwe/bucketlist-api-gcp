d035291aa76434bf89b28c1ab7f880fb91a9892d
Innocent Asiimwe <asiimwe@outlook.com>
2017-09-17 18:34:47 +0300

    Add functionality to create bucketlist

--- app/bucketlist/views.py
+++ app/bucketlist/views.py
@@ -1,9 +1,38 @@
+"""views for bucketlist_blueprint """
+from app.utils import auth_required
+from app.models import Bucketlist
 from . import bucketlist_blueprint
 from flask import make_response, jsonify, request
-from app.utils import auth_required
 
-@bucketlist_blueprint.route('', methods=['POST', 'GET'])
+@bucketlist_blueprint.route('/', methods=['POST', 'GET'])
 @auth_required
-def bucketlists(resp):
+def bucketlists(resp, auth_token):
     """view function to handle /bucketlists endpoint """
-    pass
+    user_bucketlists = Bucketlist.get_all_bucketlists(resp)
+
+    if request.data['name'] not in user_bucketlists:
+        name = request.data['name']
+        description = request.data['description']
+        owner = resp
+
+        try:
+            new_bucketlist = Bucketlist(name, description, owner)
+            new_bucketlist.save()
+            response = {
+                'status': 'Success',
+                'id': new_bucketlist.id,
+                'name': new_bucketlist.name,
+                'description': new_bucketlist.description,
+                'owner': new_bucketlist.owner
+            }
+            return make_response(jsonify(response)), 201
+        except Exception as e:
+            response = {
+                'status': 'Failed',
+                'message': str(e)
+            }
+    response = {
+        'status': 'Failed',
+        'message': 'Failed to create bucketlist, name already exists'
+    }
+    return make_response(jsonify(response)),202

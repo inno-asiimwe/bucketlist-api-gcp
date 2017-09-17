962c80ed63c3f1a02a655617a18bff15d27d5b83
Innocent Asiimwe <asiimwe@outlook.com>
2017-09-17 10:55:47 +0300

    Refactor auth_required decorator function

--- app/utils.py
+++ app/utils.py
@@ -3,11 +3,13 @@ from functools import wraps
 from flask import request, jsonify, make_response
 from .models import User
 
-def @auth_required(f):
+def auth_required(f):
+    """decorator for authenticating a user using token based authentication """
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        response  = {
-            'status': 'Failed'
+        """ Decorated function for authenticating a user"""
+        response = {
+            'status': 'Failed',
             'message': 'Invalid token'
         }
         code = 401
@@ -17,7 +19,8 @@ def @auth_required(f):
             auth_token = ''
         else:
             auth_token = auth_header.split(" ")[1]
-        
-        if not auth_token or isinstance(User.decode_auth_token(auth_token), str):
+            resp = User.decode_auth_token(auth_token)
+        if not auth_token or isinstance(resp, str):
             return make_response(jsonify(response)), code
-        return decorated_function
+        return f(resp, *args, **kwargs)
+    return decorated_function

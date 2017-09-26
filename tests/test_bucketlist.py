aec889f333fdeb844888db692d9629a4d823f272
Innocent Asiimwe <asiimwe@outlook.com>
2017-09-26 16:41:16 +0300

    Add test for search by name

--- tests/test_bucketlist.py
+++ tests/test_bucketlist.py
@@ -473,3 +473,32 @@ class TestBucketlist(BaseTestCase):
             self.assertEqual(res_bucketlist.status_code, 201)
             self.assertEqual(response_1.status_code, 201)
             self.assertIn('Success', data['status'])
+
+    def test_search_for_bucketlist(self):
+        """Tests API can search for a bucketlist by name"""
+        with self.client:
+            res_register = self.register_user()
+            res_login = self.login_user()
+            user_id = User.query.filter_by(username='inno').first().id
+            access_token = json.loads(res_login.data.decode())['auth_token']
+            res_bucketlist = self.client.post(
+                '/bucketlists/',
+                headers=dict(Authorization='Bearer ' + access_token),
+                data=json.dumps(dict(
+                    name='Before 30',
+                    description='Things to do before I am 30',
+                    owner=user_id
+                )),
+                content_type='application/json'
+            )
+            response = self.client.get(
+                '/bucketlist?q=Before 30',
+                headers=dict(Authorization='Bearer ' + access_token),
+                content_type='application/json'
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(res_register.status_code, 201)
+            self.assertEqual(res_login.status_code, 200)
+            self.assertEqual(res_bucketlist.status_code, 201)
+            self.assertEqual(response.status_code, 200)
+            self.assertIn('Before 30', data['name'] )

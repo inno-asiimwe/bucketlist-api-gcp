861cf46cdb4e82970342267e63125f5087eb1d00
Innocent Asiimwe <innocent@Innocents-MacBook-Pro.local>
2017-10-20 21:31:17 +0300

    Refactor models import in manage.py

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ import os
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from app import db, create_app
-from app import models
+from app.models import User, Bucketlist, BlacklistToken, Item
 
 app = create_app(os.getenv('APP_SETTINGS'))
 migrate = Migrate(app, db)
